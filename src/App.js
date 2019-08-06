f9773954fec275bf5b4b208e1c4cf55213d89078
Eunsoo Kim <38200754+EunsooKim@users.noreply.github.com>
2019-08-06 23:59:58 +0900

    state and lifecycle done

--- src/App.js
+++ src/App.js
@@ -1,58 +1,27 @@
 import React from 'react';
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 
-function Food({name, image, rating}){
-  return(
-    <div>
-      <h2> I like {name}</h2>
-      <h4> {rating}</h4>
-      <img src={image} alt={name} width="400"/>
-    </div>
-    );
-}
-
-Food.propTypes = {
-  name : PropTypes.string.isRequired,
-  image : PropTypes.string.isRequired,
-  rating : PropTypes.number.isRequired
-};
-
-const FoodILike = [
-  { 
-    id : 1,
-    name: "kimchi",
-    image: "https://cms.splendidtable.org/sites/default/files/styles/w2000/public/6878019331_c6d21380b9_z.jpg?itok=QPCBvQxa",
-    rating: 3.4
-  },
-  { 
-    id : 2,
-    name: "pizza",
-    image: "https://recipes.timesofindia.com/photo/53110049.cms",
-    rating: 3
-  },
-  { 
-    id : 3,
-    name: "chiken",
-    image: "https://i.ytimg.com/vi/VqWQh6bFmtM/maxresdefault.jpg",
-    rating: 5
-  },
-  { 
-    id: 4,
-    name: "kimbap",
-    image: "https://www.maangchi.com/wp-content/uploads/2007/08/gimbap_plate.jpg",
-    rating: 4
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoading: true,
+      movies: []
+    }
   }
-]
- 
 
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({ isLoading: false })
+    }, 6000);
+  }
 
-function App() {
-  return (
-    <div>
-      <h1> Hello !!!</h1>
-      {FoodILike.map((dish)=> <Food key={dish.id} name={dish.name} image={dish.image} rating={dish.rating}/>)}
-    </div>
-  );
+  render(){
+    const { isLoading } = this.state;
+    return (
+      <div>{isLoading ? "Loading..." : "We are ready"}</div>
+    );
+  }
 }
 
 export default App;
