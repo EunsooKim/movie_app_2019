1fb0f1d1d666f5155c8c378fb80f7adeda92d3fe
EunsooKim <kimessu@gmail.com>
2019-08-15 00:09:45 +0900

    Updates

---
+++ precache-manifest.cf1e8f6d66ee5fb863320dc138275475.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb4bb406b005fede0c7802efb5256785",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "e3412f0df6ec774de856",
+    "url": "/movie_app_2019/static/css/main.28bd6319.chunk.css"
+  },
+  {
+    "revision": "7cc1bb732901394da363",
+    "url": "/movie_app_2019/static/js/2.5a07769b.chunk.js"
+  },
+  {
+    "revision": "e3412f0df6ec774de856",
+    "url": "/movie_app_2019/static/js/main.e6bf1368.chunk.js"
+  },
+  {
+    "revision": "5a2b9365f7ca01d151d6",
+    "url": "/movie_app_2019/static/js/runtime~main.64ac130e.js"
+  }
+]);
